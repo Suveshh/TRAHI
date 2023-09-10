8c415b37cee835367efbe65a43724dee194adcc8
Suveshh <125795082+Suveshh@users.noreply.github.com>
2023-09-10 16:35:15 +0530

    Add files via upload

---
+++ index/hello.js
@@ -0,0 +1,10 @@
+      </div>
+      <div class="footpanel3" id="follow">
+        <h3>follow us :</h3>
+        <i class="fa-brands fa-twitter fa-beat"></i></i>
+        <i class="fa-brands fa-youtube fa-beat"></i>
+        <i class="fa-brands fa-instagram fa-beat"></i>
+        <i class="fa-brands fa-facebook fa-beat"></i></i>
+        <i class="fa-brands fa-linkedin fa-beat"></i>
+      
+      </div>
